f567636b2a820dd749c4faf14d539fa509b88d57
desireekodehode <desireekodehode1@gmail.com>
2023-09-12 14:44:44 +0200

    '

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+let randomNumberGenerator = Math.round(Math.random()* 19) + 1;
+if (randomNumberGenerator >10) {
+    console.log("Tallet er større enn 10");
+} else {
+    console.log("Tallet er 5");
+}
+console.log (`Tallet er: ${randomNumberGenerator}`);
+
+function getRandomNumberGenerator (maxNumber) {
+    return Math.round(Math.random()* maxNumber) + 1;
+}
+
+console.log()
